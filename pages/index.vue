64229f8e577f8fb56c27f2b00e5a0eefab55210f
MadhurimaMitra <madhurimamitra2016@gmail.com>
2024-06-13 16:41:51 +0530

    Changes in admin

--- pages/index.vue
+++ pages/index.vue
@@ -6,18 +6,18 @@
 </template>
 
 <script setup>
-useHead({
-  script: [
-    { src: "https://identity.netlify.com/v1/netlify-identity-widget.js" },
-  ],
-});
-if (window.netlifyIdentity) {
-    window.netlifyIdentity.on("init", (user) => {
-      if (!user) {
-        window.netlifyIdentity.on("login", () => {
-          document.location.href = "/admin/";
-        });
-      }
-    });
-  }
+// useHead({
+//   script: [
+//     { src: "https://identity.netlify.com/v1/netlify-identity-widget.js" },
+//   ],
+// });
+// if (window.netlifyIdentity) {
+//     window.netlifyIdentity.on("init", (user) => {
+//       if (!user) {
+//         window.netlifyIdentity.on("login", () => {
+//           document.location.href = "/admin/";
+//         });
+//       }
+//     });
+//   }
 </script>
