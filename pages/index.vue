792c8a3c8256802d10ca6d7cbab446f786a02df3
MadhurimaMitra <madhurimamitra2016@gmail.com>
2024-06-13 16:47:22 +0530

    Changes in index

--- pages/index.vue
+++ pages/index.vue
@@ -6,18 +6,9 @@
 </template>
 
 <script setup>
-// useHead({
-//   script: [
-//     { src: "https://identity.netlify.com/v1/netlify-identity-widget.js" },
-//   ],
-// });
-// if (window.netlifyIdentity) {
-//     window.netlifyIdentity.on("init", (user) => {
-//       if (!user) {
-//         window.netlifyIdentity.on("login", () => {
-//           document.location.href = "/admin/";
-//         });
-//       }
-//     });
-//   }
+useHead({
+  script: [
+    { src: "https://identity.netlify.com/v1/netlify-identity-widget.js" },
+  ],
+});
 </script>
