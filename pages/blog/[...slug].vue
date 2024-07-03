437d08ca9d5dfb2b2a46767ce907e4dcc4e3341a
MadhurimaMitra <madhurimamitra2016@gmail.com>
2024-07-03 20:11:56 +0530

    Changes in first article

--- pages/blog/[...slug].vue
+++ pages/blog/[...slug].vue
@@ -2,7 +2,7 @@
   <div class="bg-white text-navy py-10">
     <ContentDoc :path="`/articles/${$route.params.slug}`" v-slot="{ doc }">
       <h1 class="lg:text-5xl sm:text-4xl font-bold text-blue text-center pb-7">{{ doc.title }}</h1>
-      <ContentRenderer :value="doc" class="text-lg lg:px-80 sm:px-6 flex flex-col items-center"></ContentRenderer>
+      <ContentRenderer :value="doc" class=" lg:px-80 sm:px-6 flex flex-col items-center"></ContentRenderer>
     </ContentDoc>
   </div>
 </template>
